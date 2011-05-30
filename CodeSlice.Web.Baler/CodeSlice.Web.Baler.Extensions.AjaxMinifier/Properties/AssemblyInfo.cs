b7d362a41bab274a3e293d53d23219b237295233
James Hughes <kouphax@gmail.com>
2011-05-30 12:10:44 +0100

    Added nuspec details

--- CodeSlice.Web.Baler/CodeSlice.Web.Baler.Extensions.AjaxMinifier/Properties/AssemblyInfo.cs
+++ CodeSlice.Web.Baler/CodeSlice.Web.Baler.Extensions.AjaxMinifier/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeSlice.Web.Baler.AjaxMinifier")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Baler.Extensions.AjaxMinifier")]
+[assembly: AssemblyDescription("Baler Extension that adds Microsoft Ajax Minifer Support to bales")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CodeSlice.Web.Baler.AjaxMinifier")]
+[assembly: AssemblyCompany("James Hughes")]
+[assembly: AssemblyProduct("CodeSlice.Web.BalerExtensions.AjaxMinifier")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- CodeSlice.Web.Baler/CodeSlice.Web.Baler/Properties/AssemblyInfo.cs
+++ CodeSlice.Web.Baler/CodeSlice.Web.Baler/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeSlice.Web.Baler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Baler")]
+[assembly: AssemblyDescription("Baler is a .NET Web Resource Bunlder.  Capable to concatenating and transforms CSS and JavaScript files to lower request count and bandwidth requirements.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("James Hughes")]
 [assembly: AssemblyProduct("CodeSlice.Web.Baler")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
